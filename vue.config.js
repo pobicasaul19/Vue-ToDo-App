e072c4430b97f65b6775290e165252a5770cb467
pobicasaul19 <phoebecasaul@gmail.com>
2023-09-04 12:21:41 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    devServer: {
-      proxy: {
-        "/api": {
-          target: "https://mevn-todo-api.onrender.com/",
-        },
-      },
-    },
-  },
+  // configureWebpack: {
+  //   devServer: {
+  //     proxy: {
+  //       "/api": {
+  //         target: "https://mevn-todo-api.onrender.com/",
+  //       },
+  //     },
+  //   },
+  // },
 });
